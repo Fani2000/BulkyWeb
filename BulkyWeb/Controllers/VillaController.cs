59d923d147f5f0976acdf8c0f2bc82bb3c474c19
Keorapetse Fani <keorapetse.fani@dimensiondata.com>
2024-03-12 15:21:56 +0200

    Added the Villa model and also finished the project setup

---
+++ BulkyWeb/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var villas = _context.Villas.ToList();
+
+            return View(villas);
+        }
+    }
+}
