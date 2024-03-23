6e66395001b5f397fda01aeb965fdc1098d5efd7
Keorapetse Fani <keorapetse.fani@dimensiondata.com>
2024-03-23 06:17:21 +0200

    Added the landing page for our app

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -1,21 +1,33 @@
+using Application.Common.Interfaces;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Web.ViewModels;
 
 namespace BulkyWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            this.unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+
+            HomeVM homeVM = new HomeVM()
+            {
+                VillaList = unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
+                Nights = 1,
+                CheckInDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
